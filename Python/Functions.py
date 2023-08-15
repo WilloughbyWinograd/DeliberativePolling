8aef6b50890093f48bd902ef546a7099a2c6a14d
WilloughbyWinograd <Willoughby@Winograd.org>
2023-08-15 03:09:59 -0700

    Begun work on dataset_import

---
+++ Python/Functions.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+import os
+
+group1 = ["Treatment", "T1", "Weight 1"]
+group2 = ["Control", "T2", "Weight 2"]
+
+#####
+
+name_group1 = group1[0]
+time_group1 = group1[1]
+weight_group1 = group1[2]
+
+name_group2 = group2[0]
+time_group2 = group2[1]
+weight_group2 = group2[2]
