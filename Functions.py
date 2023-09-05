aea897c6f0e1acf5a08031b094d1a4ff3db9ccc7
WilloughbyWinograd <Willoughby@Winograd.org>
2023-09-05 13:32:39 -0700

    Turned off Word document exports.

--- Functions.py
+++ Functions.py
@@ -127,26 +127,12 @@ def analysis_tables(sample, type):
                 ),
             )
             #write_docx(sample,document_title(sample,"Tables",type,sample.metadata.column_labels[sample.metadata.column_names.index(nominal_variable)],),variable=sample.metadata.column_labels[sample.metadata.column_names.index(nominal_variable)],)
-            ordinal_report(
-                sample,
-                document_title(
-                    sample,
-                    "Report",
-                    type,
-                    sample.metadata.column_labels[
-                        sample.metadata.column_names.index(nominal_variable)
-                    ],
-                ),
-                variable=sample.metadata.column_labels[
-                    sample.metadata.column_names.index(nominal_variable)
-                ],
-            )
-            sample.crosstabs = pd.DataFrame()
+            #ordinal_report( sample, document_title( sample, "Report", type, sample.metadata.column_labels[ sample.metadata.column_names.index(nominal_variable) ], ), variable=sample.metadata.column_labels[ sample.metadata.column_names.index(nominal_variable) ], ) sample.crosstabs = pd.DataFrame()
 
     if type == "Nominal":
         write_xlsx(sample, document_title(sample, "Tables", type, nominal_variable))
         #write_docx(sample, document_title(sample, "Tables", type, nominal_variable))
-        nominal_report(sample, document_title(sample, "Report", type, sample.metadata.column_labels[sample.metadata.column_names.index(nominal_variable)],), )
+        #nominal_report(sample, document_title(sample, "Report", type, sample.metadata.column_labels[sample.metadata.column_names.index(nominal_variable)],), )
 
 
 def nominal_crosstab(sample, nominal_variable):
