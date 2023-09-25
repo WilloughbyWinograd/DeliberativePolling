aadf94886080ff5519a88bcab7677337905a76b5
WilloughbyWinograd <Willoughby@Winograd.org>
2023-09-25 13:52:27 -0700

    0.1.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
   
 setuptools.setup(
     name="DeliberativePolling",
-    version="0.1.8",
+    version="0.1.9",
     author="The Deliberative Democracy Lab at Stanford University",
     author_email="deliberation@stanford.edu",
     packages=["DeliberativePolling"],
